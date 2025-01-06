b06110cf754d6d4376c60e9b5ca8395fc750a029
kacperrzymkiewicz <kacper9800@gmail.com>
2025-01-06 23:20:49 +0100

    Fix for building on Github Actions

--- wind-farm-service/src/test/java/com/windFarm/WindFarmApplicationTests.java
+++ wind-farm-service/src/test/java/com/windFarm/WindFarmApplicationTests.java
@@ -2,8 +2,10 @@ package com.windFarm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WindFarmApplicationTests {
 
 	@Test
