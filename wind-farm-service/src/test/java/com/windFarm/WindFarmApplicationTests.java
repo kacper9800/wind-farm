0bb06b17a439168dbd8ff3f921b5867d13b643c8
kacperrzymkiewicz <kacper9800@gmail.com>
2025-01-06 23:39:46 +0100

    Fix for test containers

--- wind-farm-service/src/test/java/com/windFarm/WindFarmApplicationTests.java
+++ wind-farm-service/src/test/java/com/windFarm/WindFarmApplicationTests.java
@@ -9,21 +9,23 @@ import org.testcontainers.containers.PostgreSQLContainer;
 @ActiveProfiles("test")
 class WindFarmApplicationTests {
 
-	static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgresql:9.6.12")
-			.withDatabaseName("testdb")
-			.withUsername("testuser")
-			.withPassword("testpass");
+    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>(
+            "postgres:16-alpine"
+    )
+            .withDatabaseName("testdb")
+            .withUsername("testuser")
+            .withPassword("testpass");
 
-	static {
-		postgreSQLContainer.start();
-		System.setProperty("DB_URL", postgreSQLContainer.getJdbcUrl());
-		System.setProperty("DB_USERNAME", postgreSQLContainer.getUsername());
-		System.setProperty("DB_PASSWORD", postgreSQLContainer.getPassword());
-	}
+    static {
+        postgreSQLContainer.start();
+        System.setProperty("DB_URL", postgreSQLContainer.getJdbcUrl());
+        System.setProperty("DB_USERNAME", postgreSQLContainer.getUsername());
+        System.setProperty("DB_PASSWORD", postgreSQLContainer.getPassword());
+    }
 
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
